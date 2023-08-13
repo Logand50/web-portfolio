fa55a49f68ddf606c7c699deb39a4a9319040178
Logand50 <128848531+Logand50@users.noreply.github.com>
2023-08-13 14:50:57 -0400

    Update App.js
    Added React Responsive Carousel.

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import { Container, Navbar, Nav, Jumbotron, Row, Col } from "react-bootstrap";
 import "./App.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
-
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 
 
@@ -28,16 +31,107 @@ const About = () => (
   </section>
 );
 
-const Projects = () => (
+
+const SamplePrevArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={`slick-arrow ${'custom-prev-arrow'}`}
+      style={{ ...style, display: 'block' }}
+      onClick={onClick}
+    >
+      <FontAwesomeIcon icon={faChevronLeft} />
+    </div>
+  );
+};
+
+const SampleNextArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={`slick-arrow ${'custom-next-arrow'}`} 
+      style={{ ...style, display: 'block' }}
+      onClick={onClick}
+    >
+      <FontAwesomeIcon icon={faChevronRight} />
+    </div>
+  );
+};
+
+
+
+const ProjectCard = ({ project }) => (
+  <div className="project-card">
+    <div className="project-content">
+      <h3>{project.title}</h3>
+      <p>{project.description}</p>
+      <a href={project.githubLink}>{project.githubLink}</a>
+    </div>
+    <div className="project-image">
+      <img src={project.imageSrc} alt={project.title} />
+    </div>
+  </div>
+);
+
+const Projects = () => {
+  const projects = [
+    {
+      id: 1,
+      title: "Tic-Tac-Toe Game",
+      description: "Tic Tac Toe Game programmed in Python, using the Pygame library. The program implements a simple computer AI for the single-player mode.",
+      githubLink: "https://github.com/Logand50/TicTacToe",
+      imageSrc: "/1687734404199.jpg",
+    },
+    {
+      id: 2,
+      title: "Snake Game",
+      description: "Snake Game programmed in Python. This code presents an engaging rendition of the classic Snake game using the Pygame library.",
+      githubLink: "https://github.com/Logand50/snake_pygame",
+      imageSrc: "/Snake_game.pdf.png",
+    },
+  ];
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    swipeToSlide: true,
+    arrows: true,
+    prevArrow: <SamplePrevArrow className="custom-prev-arrow" />,
+    nextArrow: <SampleNextArrow className="custom-next-arrow" />,
+  };
+
+  return (
+    <section id="projects">
+      <h2>Projects</h2>
+      <Slider {...settings}>
+        {projects.map((project) => (
+          <div key={project.id}>
+            <ProjectCard project={project} />
+          </div>
+        ))}
+      </Slider>
+    </section>
+  );
+};
+
+
+
+
+
+
+/* const Projects = () => (
   <section id="projects">
     <h2>Projects</h2>
     <Row>
       <Col md={4}>
         <div className="project-card">
           <h3>Tic-Tac-Toe Game</h3>
-          <p>Tic-Tac-Toe with AI bot. Experimenting with simple AI programming in this Tic-Tac-Toe game. Programmed with Python.</p>
+          <p>Tic-Tac-Toe with AI bot. Experimenting with algorithims programming this Tic-Tac-Toe game. Programmed with Python.</p>
           <a href='https://github.com/Logand50/TicTacToe'>https://github.com/Logand50/TicTacToe</a>
-          <img src='https://media.licdn.com/dms/image/D562DAQFNrmTwJsGe_g/profile-treasury-image-shrink_800_800/0/1687734404199?e=1690815600&v=beta&t=f-qwJrJh2b2Gy0WmopZq1Bd6SQYUoUdqbrHr-CW1ZrE'></img>
+          <img src='/1687734404199.jpg'></img>
         </div>
       </Col>
       <Col md={4}>
@@ -45,38 +139,40 @@ const Projects = () => (
           <h3>Snake Game</h3>
           <p>Classic snake game programmed using Python and pygame. Quick project created for fun.</p>
           <a href='https://github.com/Logand50/snake_pygame'>https://github.com/Logand50/snake_pygame</a>
-          <img src='https://media.licdn.com/dms/image/D562DAQFHzzGI7EMbDA/profile-treasury-image-shrink_800_800/0/1687647063768?e=1690815600&v=beta&t=fXr7MrbBidpPaGA4Nm4LSFUHWl1bZlkxuu3cVq4SjlM'></img>
+          <img src='/Snake_game.pdf.png'></img>
 
         </div>
       </Col>
-      {/* Add more project cards as needed */}
     </Row>
   </section>
 );
-
+*/
 
 const Contact = () => (
   <section id="contact">
     <div className="contact-icons">
-    <h2>Contact</h2>
-    <p>Email: logandye50@gmail.com</p>
-    <p>Phone Number: 614-507-3783</p>
-    <p>Linkedin: <a id='contact-linkedin' href='https://www.linkedin.com/in/logan-dye-8b2b24256/'>https://www.linkedin.com/in/logan-dye-8b2b24256/</a></p>
-    <p>GitHub: <a id='contact-git' href='https://github.com/Logand50'>https://github.com/Logand50</a></p>
-    <a href="https://www.linkedin.com/in/logan-dye-8b2b24256/" target="_blank" rel="noopener noreferrer">
-        <FontAwesomeIcon icon={faLinkedin} />
-    </a>  
-    <a href="https://github.com/Logand50" target="_blank" rel="noopener noreferrer">
-        <FontAwesomeIcon icon={faGithub} />
-    </a>
-
+      <h2>Contact</h2>
+      <p>Email: logandye50@gmail.com</p>
+      <p>Phone Number: 614-507-3783</p>
+      <p>Linkedin: <a id='contact-linkedin' href='https://www.linkedin.com/in/logan-dye-8b2b24256/'>https://www.linkedin.com/in/logan-dye-8b2b24256/</a></p>
+      <p>GitHub: <a id='contact-git' href='https://github.com/Logand50'>https://github.com/Logand50</a></p>
+      <div className="icon-container">
+        <a href="https://www.linkedin.com/in/logan-dye-8b2b24256/" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faLinkedin} />
+        </a>  
+        <a href="https://github.com/Logand50" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+      </div>
     </div>
+    
+
   </section>
 );
 
 const App = () => {
   useEffect(() => {
-    // Function to handle smooth scrolling
+    // smooth scrolling
     const smoothScroll = (target) => {
       const element = document.querySelector(target);
       if (element) {
